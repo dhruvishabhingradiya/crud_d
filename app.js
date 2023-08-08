0a8734dad99741607cf0b84ad73375f412820283
dhruvishabhingradiya <dhruvishabhingradiya@gmail.com>
2023-08-08 12:50:14 +0600

    updated

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ db.once('open', () => {
 //   }
 // });
 app.use("/",(req,res)=>{
-  res.json({message:"hello i m deploy"})
+  res.json({message:"hello my name is dhruvisha"})
 })
 
 app.listen(port, function (error) {
