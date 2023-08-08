f3dbdefbbf4a20967cacdaa96a8ef13e4f16f45f
dhruvishabhingradiya <dhruvishabhingradiya@gmail.com>
2023-08-08 12:28:42 +0600

    update

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors=require("cors")
 const port = process.env.PORT || 5000
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://0.0.0.0:27017/crud', { useNewUrlParser: true, useUnifiedTopology: true });
