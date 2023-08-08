7ef68a5df71af8cc6ce3c20c6061b6a4fc901d16
dhruvishabhingradiya <dhruvishabhingradiya@gmail.com>
2023-08-08 12:45:55 +0600

    update

--- app.js
+++ app.js
@@ -17,105 +17,107 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-const crudSchema = new mongoose.Schema({
-  username: String,
-  usermail: String,
-  password: String,
-  resume: String,
-  hobbies: [String],
-  gender: String,
-  city: String,
-  address: String
-});
+// const crudSchema = new mongoose.Schema({
+//   username: String,
+//   usermail: String,
+//   password: String,
+//   resume: String,
+//   hobbies: [String],
+//   gender: String,
+//   city: String,
+//   address: String
+// });
 
-const crud = mongoose.model('crud', crudSchema);
+// const crud = mongoose.model('crud', crudSchema);
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', async (req, res) => {
-  try {
-    const users = await crud.find();
-    res.render('show', { crud: users });
-  } catch (err) {
-    console.error('Failed to fetch users:', err);
-    res.sendStatus(500);
-  }
-});
+// app.get('/', async (req, res) => {
+//   try {
+//     const users = await crud.find();
+//     res.render('show', { crud: users });
+//   } catch (err) {
+//     console.error('Failed to fetch users:', err);
+//     res.sendStatus(500);
+//   }
+// });
 
-app.post('/show', async (req, res) => {
-  try {
-    const newcrud = new crud({
-      username: req.body.username,
-      usermail: req.body.usermail,
-      password: req.body.password,
-      resume: req.body.resume,
-      hobbies: req.body.hobbies,
-      gender: req.body.gender,
-      city: req.body.city,
-      address: req.body.address
-    });
+// app.post('/show', async (req, res) => {
+//   try {
+//     const newcrud = new crud({
+//       username: req.body.username,
+//       usermail: req.body.usermail,
+//       password: req.body.password,
+//       resume: req.body.resume,
+//       hobbies: req.body.hobbies,
+//       gender: req.body.gender,
+//       city: req.body.city,
+//       address: req.body.address
+//     });
 
-    await newcrud.save();
-    res.send('User saved successfully!');
-  } catch (err) {
-    console.error('Failed to save user:', err);
-    res.sendStatus(500);
-  }
-});
+//     await newcrud.save();
+//     res.send('User saved successfully!');
+//   } catch (err) {
+//     console.error('Failed to save user:', err);
+//     res.sendStatus(500);
+//   }
+// });
 
-// ...
+// // ...
 
-app.post('/update/:name', async (req, res) => {
-  try {
-    const { name } = req.params;
+// app.post('/update/:name', async (req, res) => {
+//   try {
+//     const { name } = req.params;
 
-    // Find the user document by their name
-    const user = await crud.findOne({ username: name });
+//     // Find the user document by their name
+//     const user = await crud.findOne({ username: name });
 
-    if (!user) {
-      res.status(404).send('User not found');
-      return;
-    }
+//     if (!user) {
+//       res.status(404).send('User not found');
+//       return;
+//     }
 
-    // Update the user's information
-    user.username = req.body.username;
-    user.usermail = req.body.usermail;
-    user.password = req.body.password;
-    user.resume = req.body.resume;
-    user.hobbies = req.body.hobbies;
-    user.gender = req.body.gender;
-    user.city = req.body.city;
-    user.address = req.body.address;
+//     // Update the user's information
+//     user.username = req.body.username;
+//     user.usermail = req.body.usermail;
+//     user.password = req.body.password;
+//     user.resume = req.body.resume;
+//     user.hobbies = req.body.hobbies;
+//     user.gender = req.body.gender;
+//     user.city = req.body.city;
+//     user.address = req.body.address;
 
-    await user.save();
+//     await user.save();
 
-    res.send('User updated successfully!');
-  } catch (err) {
-    console.error('Failed to update user:', err);
-    res.sendStatus(500);
-  }
-});
+//     res.send('User updated successfully!');
+//   } catch (err) {
+//     console.error('Failed to update user:', err);
+//     res.sendStatus(500);
+//   }
+// });
 
-app.post('/delete/:name', async (req, res) => {
-  try {
-    const { name } = req.params;
+// app.post('/delete/:name', async (req, res) => {
+//   try {
+//     const { name } = req.params;
 
-    // Find the user document by their name and remove it
-    const result = await crud.deleteOne({ username: name });
+//     // Find the user document by their name and remove it
+//     const result = await crud.deleteOne({ username: name });
 
-    if (!result.deletedCount) {
-      res.status(404).send('User not found');
-      return;
-    }
+//     if (!result.deletedCount) {
+//       res.status(404).send('User not found');
+//       return;
+//     }
 
-    res.send('User deleted successfully!');
-  } catch (err) {
-    console.error('Failed to delete user:', err);
-    res.sendStatus(500);
-  }
-});
-
+//     res.send('User deleted successfully!');
+//   } catch (err) {
+//     console.error('Failed to delete user:', err);
+//     res.sendStatus(500);
+//   }
+// });
+app.use("/",(req,res)=>{
+  res.json({message:"hello i m deploy"})
+})
 
 app.listen(port, function (error) {
     if (error) throw error
